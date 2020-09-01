133d3807d00b337d786d65915cb619d09f8065d1
aamadoUCF <61637707+aamadoUCF@users.noreply.github.com>
2020-09-01 15:49:23 -0400

    Add files via upload

---
+++ api/addContact.php
@@ -0,0 +1,56 @@
+<?php
+
+$inData = getRequestInfo();
+
+
+// In our data, we need the following information
+$userId = $inData["userId"];
+$contactName = $inData["contactName"];
+$companyName = $inData["companyName"];
+$address = $inData["address"];
+$email = $inData["email"];
+$phone = $inData["phone"];
+
+// Establish connection
+$conn = new mysqli("localhost", "group4cp_admin", "!@Pass4U@!", "group4cp_corporate");
+
+// Test connection
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    // Insert necessary values.
+    $sql = "insert into CONTACTS (UserId, ContactName, CompanyName, Address, Email, Phone) "
+    . "VALUES (" . $userId . ",'" . $contactName . "','" . $companyName . "','" . $address
+    . "','" . $email . "','" . $phone . "')";
+
+    if ($result = $conn->query($sql) != TRUE)
+    {
+        returnWithError($conn->error);
+    }
+
+    $conn->close();
+}
+
+returnWithError("");
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+?>
