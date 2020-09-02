d9f5097dd2d1d0e0207c4492fd6024c40dee03f8
Andrew <aamado@knights.ucf.edu>
2020-09-01 21:42:59 -0400

    added first version of createUser api for creating an account

---
+++ api/createUser.php
@@ -0,0 +1,60 @@
+<?php
+
+$inData = getRequestInfo();
+
+// We need the following info
+$username = $inData["username"];
+$password = $inData["password"];
+$name = $inData["name"];
+$companyName = $inData["companyName"];
+$position = $inData["position"];
+$address = $inData["address"];
+$email = $inData["email"];
+$phone = $inData["phone"];
+
+// Encrypt password
+$encryptedPassword = AES_ENCRYPT($password, UNHEX(SHA2("poop", 256)));
+
+// Establish connection
+$conn = new mysqli("localhost", "group4cp_admin", "!@Pass4U@!", "group4cp_corporate")
+
+// Test connection
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    // Our SQL command.
+    $sql = "insert into USERS (Username, Password, PreferredName, CompanyName, Position, "
+    . "Email, Phone) VALUES ('" . $username . "','" . $encryptedPassword . "','" . $name
+    . "','" . $companyName . "','" . $position . "','" . $address "','" . $email . "','"
+    . $phone . "')"
+
+    // Check if our query is successful or not
+    if ($result = $conn->query($sql) != TRUE)
+    {
+        returnWithError($conn->error)
+    }
+
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+?>
