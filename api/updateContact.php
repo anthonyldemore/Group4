493165316d7f160238a43c79116cbbd8b4be5e20
Andrew <aamado@knights.ucf.edu>
2020-09-15 00:23:45 -0400

    Initial script for testing

---
+++ api/updateContact.php
@@ -0,0 +1,38 @@
+<?php
+    include 'common.php';
+
+    $inData = getRequestInfo();
+
+    // In our data, we need the following information
+    $userId = $inData["userId"];
+    $contactId = $inData["contactId"];
+    $contactName = $inData["contactName"];
+    $companyName = $inData["companyName"];
+    $address = $inData["address"];
+    $email = $inData["email"];
+    $phone = $inData["phone"];
+
+    $conn = new mysqli("localhost", "group4cp_admin", "!@Pass4U@!", "group4cp_corporate");
+
+    if ($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+    else
+    {
+        // Insert Values
+        $sql = "UPDATE `CONTACTS` "
+                . "SET ContactName = `" . $contactName . "`,"
+                . "CompanyName = `" . $companyName . "`,"
+                . "Address = `" . $address . "`,"
+                . "Email = `" . $email . "`,"
+                . "Phone = `" . $phone . "` "
+                . "WHERE `UserID` IN ($userId) AND ID = $contactId";
+
+        if (result = $conn->query($sql) != TRUE)
+        {
+            returnWithError($conn->error);
+        }
+        $conn->close();
+    }
+?>
