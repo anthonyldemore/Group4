a438fcb149a0bcf1b969cb77fe084fa0bc77953e
Wayne He <wayneh000@knights.ucf.edu>
2020-09-24 17:14:22 -0400

    CORS Attempt 2

--- api/common.php
+++ api/common.php
@@ -1,4 +1,18 @@
 <?php
+  $app = new \MySQL\App;
+
+  $app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+  });
+
+  $app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  });
+
   $key = 'poop';
 
   function getRequestInfo()
