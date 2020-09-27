bc312c666fbf9e65e8c5f61a6aae3d2b506e41c9
DanielMolares <70301465+DanielMolares@users.noreply.github.com>
2020-09-27 18:05:04 -0400

    local branch
    used to pull from master

---
+++ src/App_REMOTE_3504.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+  background: lightgrey !important;
+  min-height: 100vh;
+}
+</style>
