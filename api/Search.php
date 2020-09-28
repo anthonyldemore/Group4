c0e27ccd0a51b9b78db5723534adcf85fe1718c7
Anthony DeMore <48776362+anthonyldemore@users.noreply.github.com>
2020-09-28 13:17:19 -0400

    empty array is returned upon invalid search

--- api/Search.php
+++ api/Search.php
@@ -7,8 +7,6 @@
 	$userId = $inData["userId"];
 	$resultArr = array();
 
-	$searchCount = 0;
-
 	$conn = new mysqli("localhost", "group4cp_admin", "!@Pass4U@!", "group4cp_corporate");
 	if ($conn->connect_error)
 	{
@@ -23,6 +21,7 @@
 			while($row = $result->fetch_assoc())
 			{
 				$resultArr[] = array(
+					"ID" => $row["ID"],
 					"ContactName" => $row["ContactName"],
 					"CompanyName" => $row["CompanyName"],
 					"Address" => $row["Address"],
@@ -35,7 +34,17 @@
 		}
 		else
 		{
-			returnWithError( "No Records Found" );
+			//returnWithError( "No Records Found" );
+			$resultArr[] = array(
+				"ID" => "",
+				"ContactName" => "",
+				"CompanyName" => "",
+				"Address" => "",
+				"Email" => "",
+				"Phone" => ""
+			);
+
+			returnWithInfo(json_encode($resultArr));
 		}
 		$conn->close();
 	}
